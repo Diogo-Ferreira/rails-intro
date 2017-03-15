026abbcd56b14e503120a3d02f4e22b3def91859
Diogo-Ferreira <d.venancio.ferreira@gmail.com>
2017-03-15 14:13:53 +0100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid only for current version of Capistrano
 lock "3.8.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "rails_intro"
+set :repo_url, "git@github.com:Diogo-Ferreira/rails-intro.git"
+set :branch, "the-end"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "home/diogo/www/rails-intro"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,10 +25,26 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+  append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+ set :keep_releases, 5
+
+
+namespace :deploy do
+  after :finished, :restart_puma do
+    on roles(:web) do
+      execute :sudo, 'service puma-diogo restart'
+      execute :sudo, 'service nginx reload'
+    end
+  end
+end
+
+# source the environment variable
+prefix = "set -a; . ~/.envfile; set +a;"
+[:bundle,:rake,:rails].each do |cmd|
+  SSHKit.config.command_map.prefix[cmd].push(prefix)
+end
